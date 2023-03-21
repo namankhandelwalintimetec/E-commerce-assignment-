59795a1e8e8dcc394eae37bb712cc5efd118a2ad
namankhandelwalintimetec <123074848+namankhandelwalintimetec@users.noreply.github.com>
2023-03-21 11:46:56 +0530

    Week 1 add log in and sign up page and use routing

--- e-commerse/src/App.tsx
+++ e-commerse/src/App.tsx
@@ -1,34 +1,12 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { doc, getDoc } from "firebase/firestore";
-import { collection } from "firebase/firestore";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Authentication from "./Screens/LogIn";
-import { auth, db } from "../src/Services/Firebase/Firebaseconfiguration";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { AddProducts } from "./Screens/Addproduct";
 import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => {
-  // const [user, setUser] = useState({ user: null });
-  // useEffect(() => {
-  //   auth.onAuthStateChanged((user: any) => {
-  //     if (user) {
-  //       getDoc(doc(collection(db, "SignedUpUsersData"), user.uid)).then(
-  //         (snapshot: any) => {
-  //           setUser({
-  //             user: snapshot.data().Name,
-  //           });
-  //         }
-  //       );
-  //     } else {
-  //       setUser({
-  //         user: null,
-  //       });
-  //     }
-  //   });
-  // }, []);
-  // const [items, loading, error] = useCollectionData(collection(db, "item"));
 
   return (
     <BrowserRouter>
