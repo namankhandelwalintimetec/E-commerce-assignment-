78c8d197f022c36b35fe3eab454c078358cf3e2b
Naman <naman.khandelwal@intimetec.com>
2023-04-07 17:18:57 +0530

    add environment file and configure firebase with it and add theam provider and start writing test and also implement notification bar

--- src/Components/ItemCard/CardInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface productDataType {
-	id: string;
-	Name: string;
-	cate: string;
-	dec: string;
-	image: string;
-	price: string;
-	rating: string;
-}
--- src/Screens/Authentication/Components/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Item = styled.div`
- 
-span{
-	background:white;
-}
-`
